cd5e451d0dbb6cb0775cf03993954200fa55eae9
Ralf Konusch <rk@wobe-systems.com>
2016-01-15 16:05:13 +0100

    Convert tabs to spaces in the introduction example

--- examples/introduction-01/server/Cmd43Handler.java
+++ examples/introduction-01/server/Cmd43Handler.java
@@ -15,28 +15,27 @@ import com.tmlsidex.tml.TMLCmdDispatchIF;
 public class Cmd43Handler implements TMLCmdDispatchIF{
   
   @Override
-  public void cmdDispatchCB(TMLCmd cmd, Object data) {	
+  public void cmdDispatchCB(TMLCmd cmd, Object data) {  
 
-	int progress = 10;
-	    
-	try {
-		System.out.println("Command "+ cmd.getCmdID() +" received");
-		
-		while (progress < 100) {	
-		    //send progress reply-message to sender
-			cmd.sendProgressReply(progress);	
-	    	
-			//do something
-	    	try {
-				Thread.sleep(500);	
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-	    	
-	    	progress=progress+10;
-	    }			
-	} catch (TmlSidexException e) {
-		e.printStackTrace();
-	}    
+    int progress = 10;
+        
+    try {
+      System.out.println("Command "+ cmd.getCmdID() +" received");
+      
+      while (progress < 100) {  
+        //send progress reply-message to sender
+        cmd.sendProgressReply(progress);  
+          
+        //do something
+        try {
+          Thread.sleep(500);  
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        progress=progress+10;
+      }     
+    } catch (TmlSidexException e) {
+      e.printStackTrace();
+    }    
   }
 }
